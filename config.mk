92516b0d722b29ab5d0aacbff659373477bda4e7
André Gaul <andre@gaul.io>
2017-03-02 00:54:45 +0100

    packages: tag as legacy before introducing wizard2

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
-PACKAGES_LIST_DEFAULT=default backbone
+PACKAGES_LIST_DEFAULT=default-legacy backbone-legacy
 LEDE_SRC=https://git.lede-project.org/source.git
 LEDE_COMMIT=48461b5abc382b26edd0d2637f0f7b952bed90ba
 MAKE_ARGS=
