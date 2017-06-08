4eb381c6142c11f9965fc985f6a9f8e28e3fb531
Sven Roederer <devel-sven@geroedel.de>
2017-06-08 03:58:33 +0200

    config:  config: use LEDE with support for ubnt-erx-sfp
    - i2c-bus defined via DTS
    - i2c-peripherials defined via DTS
    - implementation of board-config for GPIOs
    
    (cherry picked from commit 7eda6e4501a0d64965cda4d6e2da5346b836911d)

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default default-legacy backbone-legacy
 LEDE_SRC=https://git.lede-project.org/source.git
-LEDE_COMMIT=9715beb04c74a918697945b3e2d23a139bd04b70
+LEDE_COMMIT=55623a9c83c8c21b25f55e63ba0b5672f074f486
 MAKE_ARGS=
