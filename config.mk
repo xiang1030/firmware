415be325eea59bd232405d61d32be5cdf8da6579
Andi Bräu <andibraeu@users.noreply.github.com>
2017-06-19 22:36:35 +0200

    use snapshot for erx-sfp

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default default-legacy backbone-legacy
 LEDE_SRC=https://git.lede-project.org/source.git
-LEDE_COMMIT=48461b5abc382b26edd0d2637f0f7b952bed90ba
+LEDE_COMMIT=9715beb04c74a918697945b3e2d23a139bd04b70
 MAKE_ARGS=
