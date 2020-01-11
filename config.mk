2b7a75ea0fe3bb2d508ca108a33c83adef7fe296
Sven Roederer <freifunk@it-solutions.geroedel.de>
2020-01-11 15:41:31 +0100

    OpenWrt and feeds: update to HEAD of master branch (31.12.2019)

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=33732f4a9c17921b782167a0dcaba9703d4e6753
+OPENWRT_COMMIT=a176f8d3ecf09df9d40aaff8271284699695d450
 SET_BUILDBOT=env
 MAKE_ARGS=
