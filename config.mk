6884a7e296c35270b290b0e87d66ff6123d93ba2
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-03-20 00:10:15 +0100

    Update to HEAD of OpenWrt-19.07 branch
    2bd9d2e08b oxnas: backport patch fixing hang after reboot

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=74a8e36975a7148b4a901b849facbb6fa309ea97
+OPENWRT_COMMIT=2bd9d2e08be4eb392b07fe42dbeea26a1e1cb7da
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
