f53bc18a6d42d79fe7ff3c5187e1bc0c810d54a7
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-03-21 00:10:14 +0100

    Update to HEAD of OpenWrt-19.07 branch
    e7fae8fc97 ath79: add support for TP-Link Archer C60 v3
    456e1c60d6 ath79: add support for TP-Link WDR3500 v1

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=2bd9d2e08be4eb392b07fe42dbeea26a1e1cb7da
+OPENWRT_COMMIT=456e1c60d61c76f21c8b8fc02473b5347c8ff247
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
