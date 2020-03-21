b7710cd472d7981570e96eb2a64d208ca700ad6d
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-03-22 00:10:13 +0100

    Update to HEAD of OpenWrt-19.07 branch
    cf4520d15e oxnas: backport another fix for irqchip

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=456e1c60d61c76f21c8b8fc02473b5347c8ff247
+OPENWRT_COMMIT=cf4520d15e6ffe884c27c9a55909774f10f1cb2c
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
