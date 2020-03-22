a8abd09c20e5fb7d1dc0adb7ab0a29c2bec11ca3
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-03-23 00:10:11 +0100

    Update to HEAD of OpenWrt-19.07 branch
    168acbb36d oxnas: yet another irqchip related patch
    798ff37aaa openssl: add configuration example for afalg-sync
    d5b1f4430f openssl: update to 1.1.1e

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=cf4520d15e6ffe884c27c9a55909774f10f1cb2c
+OPENWRT_COMMIT=d5b1f4430f5649a857485341834a48b3420fc0cc
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
