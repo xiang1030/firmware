f3cc807790da177bf92bf3e070871e19cf2af6ce
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-03-24 00:10:18 +0100

    Update to HEAD of OpenWrt-19.07 branch
    39405644d5 dnsmasq: add 'scriptarp' option

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=d5b1f4430f5649a857485341834a48b3420fc0cc
+OPENWRT_COMMIT=39405644d5b97796a633315e0ce7b65f8a1eb3ac
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
