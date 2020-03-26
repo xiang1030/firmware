65f4ce81d418533ddb649dbb8cb08740691226a5
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-03-27 00:10:15 +0100

    Update to HEAD of OpenWrt-19.07 branch
    81264ebb51 brcm2708: fix build failure
    27e77922a1 ar71xx: use status led for GL.iNet GL-AR750S

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=39405644d5b97796a633315e0ce7b65f8a1eb3ac
+OPENWRT_COMMIT=27e77922a1cbd6c93fe618d9f296a7987a4879c3
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
