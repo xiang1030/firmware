af1cad5441bc76de0805f704a0b77ed9e678e848
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-03-29 00:10:12 +0100

    Update to HEAD of OpenWrt-19.07 branch
    f5b3cd1539 ar71xx: Fix gigabit switch support for Mikrotik RB951G-2HnD
    794fd4c6cf procd: turn error into debug message for missing ujail binary

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=bdbda30384980ed545391ddf23308ca4bde34770
+OPENWRT_COMMIT=794fd4c6cf25dfe08ecd63ddd2d105d1fe80b601
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
