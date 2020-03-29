5bd2fa8229ff3be185680c21b43a9f87bc977257
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-03-30 00:10:11 +0200

    Update to HEAD of OpenWrt-19.07 branch
    c6c3f6bb0a mac80211: Update to version 4.19.112
    eea3a9625c openssl: revert EOF detection change in 1.1.1
    66cbfeeaae build: add GCC 10 version detection
    a08394b3c6 build: prereq: tidy gcc version checks
    79b60d878d squashfskit4/Makefile: introduce PKG_RELEASE=1
    45b586c4a6 tools: squashfskit4: fix build with GCC10
    83381ce95d readline: needs host depend on ncurses to build
    bf5ea2a8dc rpcd: fix respawn settings
    96092a8eea mkrasimage: fix segmentation fault
    96ee7c8bfd libpcap: Update shared-lib patch from Debian to fix linking problems

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=794fd4c6cf25dfe08ecd63ddd2d105d1fe80b601
+OPENWRT_COMMIT=96ee7c8bfddd5a7fb34281aeffb88ed56c1d7b68
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
