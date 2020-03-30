397f22443a5816b841fbb34ade1192620346e055
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-03-31 00:10:11 +0200

    Update to HEAD of OpenWrt-19.07 branch
    470f7c046c ath79: add support for TP-Link TL-WDR4310 v1

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=96ee7c8bfddd5a7fb34281aeffb88ed56c1d7b68
+OPENWRT_COMMIT=470f7c046c54c4db9914e98aabf705b5856fd693
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
