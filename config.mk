028c18e04e3c25d101d0fa1b0d07a1771b3a9e27
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-02 00:10:19 +0200

    Update to HEAD of OpenWrt-19.07 branch
    36373c5ddb openssl: bump to 1.1.1f

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=470f7c046c54c4db9914e98aabf705b5856fd693
+OPENWRT_COMMIT=36373c5ddb1b6fc81fa239a1f898c5abc3f24f40
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
