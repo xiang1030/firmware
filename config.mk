2f8d3755f60acf1f46c4c84d965be9a20c2db5fd
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-08 00:10:13 +0200

    Update to HEAD of OpenWrt-19.07 branch
    a89731ad7a bcm53xx: fix ASUS firmwares to use vendor format
    55c29c398c busybox: enable truncate on bcm53xx target
    35413b047c bcm53xx: sysupgrade: optimize building UBI image
    ab3549a870 bcm53xx: refactor board.d code in 02_network
    5b9b833f8c bcm53xx: add support for Luxul FullMAC WiFi devices

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=36373c5ddb1b6fc81fa239a1f898c5abc3f24f40
+OPENWRT_COMMIT=5b9b833f8c4dc973f557e90f1038d7d3f1d2042b
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
