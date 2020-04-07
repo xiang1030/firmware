9b3733ca546d6bad43375a8d5783787fe2f0fddb
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-08 00:20:12 +0200

    Update to HEAD of OpenWrt-master
    7daab62861 ath79: add support for Comfast CF-EW72
    c459a6bf48 bcm53xx: add support for Luxul FullMAC WiFi devices
    e8787291b3 ramips: add support for Buffalo WSR-2533DHPL
    92792ead34 kmod: add netlink-diag package
    d8bde3687a iproute2: add kmod-netlink-diag for ss

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=ae06a650d68026530beec4196869706866c2e4b1
+OPENWRT_COMMIT=d8bde3687a23742d1072be1a55f3b44ae29438ae
 SET_BUILDBOT=env
 MAKE_ARGS=
