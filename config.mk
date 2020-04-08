fd32961270e53bdef13b9d11e7ec96dc69a1722a
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-09 00:20:14 +0200

    Update to HEAD of OpenWrt-master
    57f4eab677 ramips: mt7621: add label mac address to rbm11g
    48b4aab80c ramips: mt7621: reenable rbm11g
    f761f4052c ramips: mt7621: harmonize naming scheme for Mikrotik
    aff965a415 generic: backport Wistron Neweb D19Q1 patches
    5410a8e295 ramips: mt7620: add rgmii delays support
    5a5a9b7a75 packages: mt76: add 14c3:7610 pci id to list
    c948a4782b ramips: add support for D-Link DWR-960
    85aef6f627 ath79: fix MAC addresses for ethernet on ZyXEL NBG6716
    fea232ae8f ramips: use full 8MB flash on ZyXEL Keenetic
    91c61aae20 base-files: add enabled commands to service rc.common
    2bdd669752 ipq806x: switch to 5.4 kernel
    9aa3d5b345 linux-atm: Include linux/sockios.h for SIOCGSTAMP
    780264980b gemini: Refine package list
    196cab719f kernel: generic: 5.4: fix mips command line parameter patch
    b299002877 kernel: bump 5.4 to 5.4.31

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=d8bde3687a23742d1072be1a55f3b44ae29438ae
+OPENWRT_COMMIT=b299002877c801202c3b61d5e09865e88f506176
 SET_BUILDBOT=env
 MAKE_ARGS=
