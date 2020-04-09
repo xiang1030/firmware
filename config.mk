7c16848334eb0318fb575228c28ca37d256877c7
Sven Roederer <freifunk@it-solutions.geroedel.de>
2020-04-10 00:46:47 +0200

    build: define upstream-repos in modules-file
    Switch from defining the upstream-repos and relating commit in config.mk
    and feeds.conf to define all in the modules-file.
    The modules-files is the real file used by the build-system to setup the
    working checkout. It originates from the Gluon-buildsystemand some
    documentation is avail at https://gluon.readthedocs.io/en/latest/dev/build.html
    
    This also relates to
    * PR https://github.com/freifunk-berlin/firmware/pull/781
    * Issue https://github.com/freifunk-berlin/firmware/issues/780
    * Issue https://github.com/freifunk-berlin/firmware/issues/772

--- config.mk
+++ config.mk
@@ -2,7 +2,5 @@
 SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
-OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=5019a06fc1027fad35fc9847735218c1057f8483
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
