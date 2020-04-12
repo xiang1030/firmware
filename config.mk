9cbf6effa5dacfb7fb061a7889962c2930e17114
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-13 00:20:11 +0200

    Update to HEAD of OpenWrt-master
    e66becb490 ramips: add support for I-O DATA WN-AX2033GR
    d761b9f211 broadcom-wl: fix compilation with kernel >= 4.15
    f8f1dc6eab bcm63xx: copy files to kernel 5.4
    04c07e05a1 bcm63xx: refresh patches for kernel 5.4
    f0fbc47a91 bcm63xx: update config for kernel 5.4
    6c521f6828 bcm63xx: fix missing watchdog core dependency
    23d3fafd87 broadcom-wl: fix compilation with kernel 5.4
    43d3ef91f1 bcm63xx: prevent fall-through in kernel patches for 5.4
    8a2118e1b4 bcm63xx: 5.4: add removed helper syscon_regmap_lookup_by_pdevname
    cb2f0df784 bcm63xx: 5.4: fix compilation for changed driver_find_device
    51b07e782d bcm63xx: use kernel 5.4 as testing kernel
    bebc9809d1 bcm63xx: remove support for kernel 4.19
    e8931b309f ramips: mt7621: tidy up names for Ubiquiti devices
    de3e5de8cc cns3xx: remove support for kernel 4.14
    282e817350 base-files: do not source system.sh in functions.sh
    ae636effd2 base-files: source functions.sh in /lib/functions/system.sh
    4f34e430ed dnsmasq: bump to v2.81
    dfa521f129 generic: spi-nor: rework broken-flash-reset
    5c8f2c64d7 ramips: enable broken-flash-reset for some 32M flash boards
    b0ec8d0ca0 ramips: increase spi frequency for newifi d1/d2
    33d027c5b8 ramips: move and rename out-of-tree mtk eth driver
    05dee5833a ramips: ralink-eth: fix device struct passed to dma functions
    6fcba5eec3 ramips: port 0034-NET-multi-phy-support.patch to 5.4
    89bf2b18af ramips: copy kernel config for 5.4
    e320435a6a ramips: refresh kernel config for 5.4
    0b68d081eb ramips: mt7628: update dts for upstream gpio-mt7621 driver
    e29e2a9484 ramips: ralink-eth: add support for 5.4 kernel
    d7d46da938 ramips: disable images for 4M devices
    abadee5d60 ramips: mt76x8: switch to kernel 5.4
    605a7ffff2 ramips: add 5.4 as testing kernel
    f017f617ae base-files: preinit: also config switch when no port roles defined
    8f6334eb94 ramips: explicitly disable built-in switch when needed
    b756ea2a90 ramips: replace pinctrl property names
    4e535d81ee bcm63xx: disable all devices with <= 4MiB flash and <= 32 MiB RAM
    0aa2ecf5b2 base-files: don't ship local build key when on buildbot

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=7b1d809a8d8620032553845cf4a8819a19508687
+OPENWRT_COMMIT=0aa2ecf5b223b838678b61bd2ded9f29903f17b1
 SET_BUILDBOT=env
 MAKE_ARGS=
