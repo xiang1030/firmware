a6d2520dccbb7ef17ca1ed28eefbb2a23355f9d7
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-13 00:10:13 +0200

    Update to HEAD of OpenWrt-19.07 branch
    14c8ea0245 ramips: use full 8MB flash on ZyXEL Keenetic
    01b624e28e Revert "ramips: disable ZyXel Keenetic by default"

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=5b9b833f8c4dc973f557e90f1038d7d3f1d2042b
+OPENWRT_COMMIT=01b624e28ec7e45573e6c501125510c19f64c257
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
