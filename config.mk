8bd7ae80f75824415e9375a7913d9b51d6b6dacb
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-14 00:20:12 +0200

    Update to HEAD of OpenWrt-master
    4d06229242 ramips: ralink-eth: fix leftover dma dev argument
    77b99ea3c6 ramips: fix whitespace in 990_NET-no-auto-carrier-support.patch
    97c5fb4709 cypress-firmware: update to v5.4.18-2020_0402
    f5a9181d6e ath79: fix wmac initialization for Zyxel NBG6716
    97af28d23e ath79: fix USB port LED assignment for ZyXEL NBG6716
    8f342a39de kernel: hwmon: add dme1737 driver
    93a8cdf5d8 mwlwifi: Update the 88W8964's firmware to 9.3.2.12 and fix backports version detection
    dce97df740 wpa_supplicant: disable CONFIG_WRITE functionality
    b43023b7ba kernel: remove non-existant symbols
    76d22fc24b hostapd: backport usleep patch
    d27623b542 elfutils: update to 0.179
    1647790ae6 toolchain/gcc: remove uclibc hack
    85ef69b202 mvebu: add support for Buffalo LinkStation LS421DE
    70193703a9 mvebu: copy files and patches to 5.4
    ae183c128f mvebu: set kernel testing version to 5.4
    d13fe4127c mvebu: refresh patches
    f72a13b537 mvebu: refresh config
    779a1c84ea mvebu: drop armada-37xx PCI aardvark patches
    35f208da3c x86: add nforce eth to default packages

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=0aa2ecf5b223b838678b61bd2ded9f29903f17b1
+OPENWRT_COMMIT=35f208da3c13cc16227f229f8aea14c4f0a06928
 SET_BUILDBOT=env
 MAKE_ARGS=
