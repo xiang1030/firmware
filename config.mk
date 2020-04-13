60e62531a2d1a8761d8e6bd5ce206232263e2b19
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-14 00:10:14 +0200

    Update to HEAD of OpenWrt-19.07 branch
    02c6deab8c mbedtls: update to version 2.16.5

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=01b624e28ec7e45573e6c501125510c19f64c257
+OPENWRT_COMMIT=02c6deab8ca88785246e505fbc953389d23548c8
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
