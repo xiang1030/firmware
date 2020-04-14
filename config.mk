cb9d79209bbfdac1223adaa8c6e19ffa9d740336
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-15 00:20:12 +0200

    Update to HEAD of OpenWrt-master
    f37d634236 hostapd: reduce to a single instance per service
    a66efbf916 mac80211: adapt for single-instance wpad
    4e4f7c6d2d netifd: network namespace jail improvements
    a5a90a94ce netifd: fix jail ifdown and jails without jail_ifname
    b3f9842330 ath79: add read support using spi-mem
    e430dfdd69 ath79: enable m25p,fast-read for tplink,tl-wr2543-v1
    1b2c4af039 ath79: enable m25p,fast-read for tplink,tl-wr1043-v1
    111029bea7 ramips: add missing DEVICE_VARS for lzma-loader
    d9e9a0e6b1 ramips: define lzma loader platform in target
    75f19deb3a ramips: define image recipe for uncompressed uimage with loader
    9fd36f54f5 Revert "kmod-sched: add act_police"
    d0cb6e995b ath79: further fixes for ZyXEL NBG6716
    e23de62845 netifd: clean up netns functionality
    7c2e0fa586 procd: jail fixes and improvements
    9abf01246e ipq806x: add patch to fix broken buttons
    0bea89a1d0 kernel: bump 5.4 to 5.4.32
    14cbd8fb2d scripts: JSON merge don't crash if no JSON found
    c737a9ee6a scripts/download: add sources CDN as first mirror

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=35f208da3c13cc16227f229f8aea14c4f0a06928
+OPENWRT_COMMIT=c737a9ee6a9c47b6e553ac81bf293b1161e59799
 SET_BUILDBOT=env
 MAKE_ARGS=
