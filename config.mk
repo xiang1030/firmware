6552162c11079e009a077f769553616f4cce6762
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-16 00:20:11 +0200

    Update to HEAD of OpenWrt-master
    f814121600 x86: append metadata to combined images
    ad19751edc ramips: mt7621: enable lzma-loader for some devices
    508462a399 ath79: add SUPPORTED_DEVICES for TP-Link TL-WA901ND v2
    a7423fef32 ath79: improve status LED definitions for GL-AR750

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=c737a9ee6a9c47b6e553ac81bf293b1161e59799
+OPENWRT_COMMIT=a7423fef3206283ec09144932b04feff7b68461f
 SET_BUILDBOT=env
 MAKE_ARGS=
