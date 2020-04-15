4efaeb1938b39a18f34b040b344d14b67b5b7c64
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-16 00:10:12 +0200

    Update to HEAD of OpenWrt-19.07 branch
    286c407c3d ath79: add SUPPORTED_DEVICES for TP-Link TL-WA901ND v2

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=02c6deab8ca88785246e505fbc953389d23548c8
+OPENWRT_COMMIT=286c407c3d8877e51fa90cbd830f2fedb3ad1f26
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
