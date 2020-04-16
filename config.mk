52e0b9ffdbd02a7a36ef8d8f11d77d76eae686aa
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-17 00:20:11 +0200

    Update to HEAD of OpenWrt-master
    e31d158c4d kernel: bump 4.14 to 4.14.176
    3c3825436e kernel: bump 4.19 to 4.19.115
    20efd5614a ath79: MikroTik: fix missing nand on kernel 5.4
    c0430b8da4 ath79: reduce spi-max-frequency for Mikrotik wAP G-5HacT2HnD
    75ef28be59 kernel: add support for GD25D05 SPI NOR (5.4)

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=a7423fef3206283ec09144932b04feff7b68461f
+OPENWRT_COMMIT=75ef28be59c5b76d73313cfb9650f464ae943cc8
 SET_BUILDBOT=env
 MAKE_ARGS=
