fb9070d5546414cb2f1d6e94a5d46dfcc912ffd7
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-18 00:20:12 +0200

    Update to HEAD of OpenWrt-master
    99d567a83d mac80211: fix detecting existing interface
    d883eaacd4 ath79: add QCA9550 reset sequence
    8918c038f3 ath79: add support for AVM FRITZ!WLAN Repeater 1750E
    0f1b5ce2f5 mac80211: drop data frames without key on encrypted links
    260a225ba4 uboot-imx6: bump to 2020.04 release
    8e99bbda19 uboot-sunxi: bump to 2020.04 relase
    16ad4de2c0 elfutils: aarch64 fix build on musl
    edf812e25c ath79: remove stray pipe
    0495324b9b mac80211: make sure existing iface belongs to correct (fullmac) phy
    23916bca61 kernel: bump 5.4 to 5.4.33
    02fcbe2f3d mbedtls: update to 2.16.6

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=75ef28be59c5b76d73313cfb9650f464ae943cc8
+OPENWRT_COMMIT=02fcbe2f3d4eaf65e90bb167aa7818eacc08c633
 SET_BUILDBOT=env
 MAKE_ARGS=
