23f91e5deb03389f45b3ac576ddce4f1df6057a8
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-19 00:20:11 +0200

    Update to HEAD of OpenWrt-master
    4745969ad7 generic: spi-nor: fix 4-byte opcode support for w25q256
    7dc82528a2 ramips: increase HiWiFi HC5962 kernel partition to 4M
    d74fb0088c ramips: use all reserved space for HiWiFi HC5962
    2fac1322f7 ramips: mt7621: Ubiquiti ER-X: fix gpio number for POE enable gpio
    d682dcc939 ramips: mt7621: Ubiquiti ER-X-SFP: fix gpio numbers for POE enable gpios
    9169482f64 ramips: add support for ipTIME A1004ns
    13a185bf8a ramips: increase spi-max-frequency for ipTIME mt7620 devices
    51c6b14092 ramips: mt7621: backport more pcie driver fixes
    1e5d014ba2 ramips: don't reuse KERNEL_DTB for lzma-loader
    19d9db5a96 ramips: mt7621: use lzma-loader for newifi d1/d2/thunder timecloud
    5f126c541a binutils: add ALTERNATIVES for strings (FS#3001)
    d7e98bd7c5 openvpn: update to 2.4.9
    d27e2c67ed bcm63xx: switch to 5.4 kernel
    7637b84fde busybox: backport Remove stime() function calls
    70a962ca6f upgs: Remove extra _DEFAULT_SOURCE definition
    14c59a147c rbcfg: Add missing mode to open call
    ce1798e915 dante: Fix compile with glibc
    02c5019a35 toolchain: glibc: Update glibc to version 2.31
    a2cf87a7b1 toolchain: glibc: Define minimum support kernel version as 4.14

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=02fcbe2f3d4eaf65e90bb167aa7818eacc08c633
+OPENWRT_COMMIT=a2cf87a7b1fa1566e2f8f6e2916b098339c19f71
 SET_BUILDBOT=env
 MAKE_ARGS=
