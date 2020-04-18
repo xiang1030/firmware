62e8cd382829d0e56452857c4e27af25d5f993ee
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-19 00:10:11 +0200

    Update to HEAD of OpenWrt-19.07 branch
    d8e0b015e8 mvebu: backport ClearFog SPI enablement
    dee8fcfe9f tegra: correct cpu subtype
    67ed408af2 mvebu: cortexa9: correct cpu subtype
    3b6f079d8d mbedtls: update to 2.16.6
    55312cc202 binutils: add ALTERNATIVES for strings (FS#3001)

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=0232f57e1af6580542c0ed1ce1d76c7cd4084613
+OPENWRT_COMMIT=55312cc2025035cc1361417eecfacb4dae46bb67
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
