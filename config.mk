3d471d53193f5785634288b3a65646850fdb5e09
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-20 00:20:13 +0200

    Update to HEAD of OpenWrt-master
    45e2b7763f ramips: replace pinctrl property names for ipTIME A1004ns
    8d9c1087e4 ath79: add support for AVM FRITZ!WLAN Repeater 450E

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=a2cf87a7b1fa1566e2f8f6e2916b098339c19f71
+OPENWRT_COMMIT=8d9c1087e4d07d625a9fef61d5dce71d850732ae
 SET_BUILDBOT=env
 MAKE_ARGS=
