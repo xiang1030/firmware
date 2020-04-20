27fbf0843d273b97c15b5b5afc488f2b94a94a4e
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-21 00:20:13 +0200

    Update to HEAD of OpenWrt-master
    7e9b56fde2 procd: fix jail when running on glibc
    08f5cac6fb ramips: mt7621: add NETGEAR R7200 as DEVICE_ALT1
    533da61ac6 umdns: update to version 2020-04-20
    be172e663f relayd: bump to version 2020-04-20
    29a458b0ca kernel: netfilter.mk: fix kmod-ipt-nat6 installation on 5.4
    6cafea5c5e uboot-kirkwood: update to 2020.04
    79d7109225 arm-trusted-firmware-rockchip: add new package
    7975060116 uboot-rockchip: add new package
    6a05a85dcb rockchip: add support for Pine64 RockPro64
    97673d8771 uboot-rockchip: fix ident string
    282e7862b7 x86: image: drop dead code
    fd94d03ae6 x86: fix kmod-forcedeth package selection
    eca6946447 x86: select kmod-button-hotplug by default
    9c23ecee57 x86: move packages selection to profiles
    be6543c539 x86: really remove packages already enabled in kconfig
    4298f0878f ubus: update to latest git HEAD
    36f628910b lantiq/fritz 7312: set maximum speed to 100 mbit
    b77fd0d30b base-files: ensure VERBOSE is set

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=8d9c1087e4d07d625a9fef61d5dce71d850732ae
+OPENWRT_COMMIT=b77fd0d30bfa93def97fc1633d389b994600fea3
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
