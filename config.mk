3d078ce3ddb4b60f4ccc0302859af3e593e79417
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-21 00:10:17 +0200

    Update to HEAD of OpenWrt-19.07 branch
    ef3df27507 umdns: suppress address-of-packed-member warning
    b6d8119c53 umdns: update to the version 2020-04-05
    b71c7c261b umdns: update to version 2020-04-20
    1df49d98e7 relayd: bump to version 2020-04-20

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=55312cc2025035cc1361417eecfacb4dae46bb67
+OPENWRT_COMMIT=1df49d98e7ed8e8e889d571973fac062694531af
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
