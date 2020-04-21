e1a50790823ff93ed9747211bd1280f0312d5971
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-22 00:20:18 +0200

    Update to HEAD of OpenWrt-master
    8d9b36270b imx6: refresh kernel config
    faf668be35 kernel: bump 5.4 to 5.4.34
    3773ae127a openssl: bump to 1.1.1g

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=b77fd0d30bfa93def97fc1633d389b994600fea3
+OPENWRT_COMMIT=3773ae127ac83766028f767ac744e87a7ddcaf50
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
