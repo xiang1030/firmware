43b8e42635c8547e34946b66603b628476ecd2ee
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-24 00:20:12 +0200

    Update to HEAD of OpenWrt-master
    91594cb137 octeon: copy files to kernel 5.4
    655c14e88c octeon: refresh patches for kernel 5.4
    2095a4e276 octeon: update config for kernel 5.4
    f6573ec24e octeon: use kernel version 5.4 for testing
    58bec0d172 ramips: remove config-4.14 for mt7621/mt76x8

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=df27e949fbbf13e1e2ab4db49f608165ef0ba9fe
+OPENWRT_COMMIT=58bec0d172fb7fc1146bdcdcd0f75b6644890fb6
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
