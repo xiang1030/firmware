5bb69b60915db0714b6384468217d839e5e0ec3e
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-25 00:20:14 +0200

    Update to HEAD of OpenWrt-master
    7b2731a691 kernel: replace "+@" IPV6 dependency by "+"
    5f92349841 kernel: fix include of myloader.o since kernel 5.4
    d3c9630811 ath79: move tplinkpart.c parser to patches
    d8b3f53f5e ltq-adsl(-fw): Makefile clean-up and fixes
    c0a80b7125 ath79: rename qca9557.dtsi to qca955x.dtsi
    fceef288cf ath79: correct QCA9550 machine compatible binding
    1f45ed6c99 ath79: fix QCA953x DDR and GPIO compatible bindings
    9fac9168c6 ipq40xx: fix alphabetical order in 02_network

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=58bec0d172fb7fc1146bdcdcd0f75b6644890fb6
+OPENWRT_COMMIT=9fac9168c6471b743dbaac5cd038593f9f807e9d
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
