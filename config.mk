31112f211fef33b4a7e1d489371d876a34044f49
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-26 00:20:14 +0200

    Update to HEAD of OpenWrt-master
    471b8bf8c1 procd: extend requirejail attribute handling
    4a3e0f736f imx6: add back perf monitor related config symbol
    ef5d37ba5e kernel: bump 5.4 to 5.4.35
    63696bd0fb kernel: backport support for clause 37 1000Base-X auto-negotiation
    6d924706a6 ramips: phy: at803x: support RGMII-to-SerDes and SFP support
    ec2f7a47d3 kernel: add module to support SFP cages
    4c70bb4f90 ramips: enable SFP port for Ubiquiti ER-X-SFP

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=9fac9168c6471b743dbaac5cd038593f9f807e9d
+OPENWRT_COMMIT=4c70bb4f906b875aee1da6636aa7d2023ef69064
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
