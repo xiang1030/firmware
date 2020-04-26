ea9da7290d366909d638c973f4ac6913935e6d63
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-27 00:20:12 +0200

    Update to HEAD of OpenWrt-master
    b23f72b5b6 kernel: netdev: add kmod-phylink
    71d5a0d92b kernel: netdev: add phylink dependency for sfp
    b359a6b948 kernel: netdev: fix kmod-sfp description capitalization
    97d86426e2 x86: fix unusable squashfs images by adding missing padding
    9f7c8ed078 umdns: update to version 2020-04-25
    9e7d11f3e2 relayd: bump to version 2020-04-25
    822c342d09 kernel: netdev: fix kmod-sfp symbols
    01961f163d oxnas: move service file to correct place
    d16a486766 ramips: fix SUPPORTED_DEVICES for Mercury MAC1200R v2
    1dedad2a00 ramips: remove unnecessary DEVICE_PACKAGES for Belkin F7C027
    b0416c9c12 gdb: disable gdbserver for arc
    80f128d2aa perf: build with NO_LIBCAP=1
    066ec97167 mac80211: ath10k: increase rx buffer size to 2048
    042917f1c4 tools/mkimage: update to 2020.04
    60712bd536 u-boot.mk: switch download to https
    b397df51aa uboot-envtools: update to 2020.04
    75512fe0e7 uboot-envtools: fix domywifi_dw33d Bad CRC error
    ee500186a5 kernel: backport fix for non-regular inodes on f2fs
    5d81b28a82 kernel: support intel X7xx 10/40GbE adapters
    cf22c4435e toolchain/gcc: eliminate uClibc atexit hack
    050c24f05c mvebu: add support for GL.iNet GL-MV1000
    ad697aabc3 bcm53xx: 5.4: enable NAND

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=4c70bb4f906b875aee1da6636aa7d2023ef69064
+OPENWRT_COMMIT=ad697aabc30875bd078275f59e69667279aa075d
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
