6b6e35738802d0fe42b09ea0b93862932ace311c
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-28 00:20:12 +0200

    Update to HEAD of OpenWrt-master
    3fdb08681b mvebu: tidy up support for GL.iNet GL-MV1000
    3f5997fb28 ath79: remove unused TP-Link partition parser
    5358d8b995 ath79: ag71xx: remove code for legacy kernels
    c760469918 ath79: ag71xx: unify version dependent code
    654eec5a2d ath79: enable SGMII workaround for affected boards
    ce19571004 ramips: mt7621: use lzma-loader for D-Link DIR-860L B1
    a25bb8610f ramips: use lzma-loader for Japanese mt7621 devices
    c998ae7f0e ramips: use lzma-loader for I-O DATA mt7621 devices
    1c008b61bd mt76: update to the latest version
    8fdf77878f mediatek: fix compile error caused by upstreamed mtk_m32 function
    1553f58a98 ramips: create common definition for I-O DATA NAND devices
    a1693bf626 ramips: explicitly disable built-in switch for lan-only devices
    40325abe52 ramips: remove (kmod-)swconfig DEVICE_PACKAGES for Sitecom WL-351
    f0435d5343 ramips: rt3883: remove swconfig from individual DEVICE_PACKAGES
    e210fe91b1 ath79: add support for Ubiquiti PowerBeam 5AC Gen2
    80a094aaf3 ath79: indicate boot/failsafe/upgrade for NanoBeam/Nanostation AC

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=ad697aabc30875bd078275f59e69667279aa075d
+OPENWRT_COMMIT=80a094aaf3247059846e7d857c236b4fa9e497c7
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
