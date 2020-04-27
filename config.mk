e6d5e6df9ff25d6aaf6ac4a2cc82eca5439e0e74
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-28 00:10:12 +0200

    Update to HEAD of OpenWrt-19.07 branch
    4e5a29827f umdns: update to version 2020-04-25
    5b4e4a38d8 relayd: bump to version 2020-04-25
    bfe652c5bd oxnas: move service file to correct place
    b80a98ae7f ramips: remove unnecessary DEVICE_PACKAGES for Belkin F7C027

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=c2efc973d593e77db84091b4bc71cdb7b412b189
+OPENWRT_COMMIT=b80a98ae7fc46437e13e4c0a2056f3d19603fc03
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
