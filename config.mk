ffe9d53a1aa687f77bc74f0c1c73fac05a0162f7
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-29 00:20:12 +0200

    Update to HEAD of OpenWrt-master
    75f7f7eee9 omap: copy kernel 4.19 config for 5.4
    d20cfa2bd3 omap: make kernel_oldconfig
    1e3ee247be omap: switch to kernel 5.4
    87c909e969 omap: remove support for kernel 4.14
    76a0ddf130 wireless-regdb: backport three upstream fixes
    a3079fb7ba elfutils: powerpc build fix
    ce2bd2bd03 ath10k-ct: remove old patches
    658e68f85c ath10k-firmware: move CT firmwares to new package
    2e5e9b459e ath10k-ct-firmware: rename ct-htt packages
    06f510df6e ath10k-ct-firmware: update firmware images
    a882bfce05 ath10k-ct-firmware: add htt-mgt variants
    a10537f674 ramips: fix MikroTik 750Gr3 ports MAC addresses
    4b3b8ec81c wpad-wolfssl: fix crypto_bignum_sub()
    4fd7e539e4 kirkwood: switch from swconfig to dsa
    0af2c44604 kirkwood: remove support for kernel 4.14
    8b373892d3 kirkwood: remove support for kernel 4.19
    03e742dcd8 Revert "ramips: explicitly disable built-in switch for lan-only devices"
    522f6b7eee ath79: add SUPPORTED_DEVICES based on ar71xx for some devices

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=80a094aaf3247059846e7d857c236b4fa9e497c7
+OPENWRT_COMMIT=522f6b7eee479768dd7851e83c4530c3329b9f53
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
