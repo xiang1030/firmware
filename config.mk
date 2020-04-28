4d3dc22d370d69f013ef885fba14c6eb8be66eb9
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-29 00:10:11 +0200

    Update to HEAD of OpenWrt-19.07 branch
    5feb0df9bb ramips: remove memory node for ZBT MT7621 devices

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=b80a98ae7fc46437e13e4c0a2056f3d19603fc03
+OPENWRT_COMMIT=5feb0df9bb3bd83ec49af17d2abc261fc58e2551
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
