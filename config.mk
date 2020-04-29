e2a069e21db117a653109b132aa638427a2c162d
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-04-30 00:20:10 +0200

    Update to HEAD of OpenWrt-master
    35a8657a40 build: prereq: change minimum make version to 3.82
    3f660249e6 kernel: hwmon: add Analog Devices AD741x support
    d9ff499671 kirkwood: disable image for Linksys EA3500 (Audi)
    447d335d00 ugps: nmea: fix time comparision
    c8b309b53d ath79: split Teltonika RUT955 to support other RUT9xx models
    aa4a33ac62 ath79: add support for Teltonika RUT955 H7V3C0

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=522f6b7eee479768dd7851e83c4530c3329b9f53
+OPENWRT_COMMIT=aa4a33ac6256fadd7201908dad1be9659b942ecd
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
