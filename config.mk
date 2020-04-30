43ea44fd523e8b3e3e1fdb83ce2099fd2ba29b0b
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-05-01 00:20:18 +0200

    Update to HEAD of OpenWrt-master
    aa0526b5f2 ath79: fix dts of Teltonika RUT9xx devices
    b17a5a9bdb dnsmasq: always inform about disabled dhcp service
    8036e7ebb5 ath10k-ct: update to version 2020-04-29
    493eef5b27 wireless-regdb: bump to latest release 2020-04-29
    5ea3ea154f kernel: bump 5.4 to 5.4.36
    f57230c4e6 wireguard: bump to 1.0.20200429
    2855be3151 uci: update to latest git HEAD
    ad27c133eb base-files: generate config files with correct permissions

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=aa4a33ac6256fadd7201908dad1be9659b942ecd
+OPENWRT_COMMIT=ad27c133eb606ee39fa346b7bdb26c1764ee869e
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
