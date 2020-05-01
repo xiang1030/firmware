aa0e4cf697b2d7ecefe9536b5805db8c741413d7
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-05-02 00:10:14 +0200

    Update to HEAD of OpenWrt-19.07 branch
    35ea808b97 uboot-kirkwood: fix ethernet and usb
    55591e63bc curl: backport fix for CVE-2019-15601
    54b6683390 wireless-regdb: backport three upstream fixes
    f141cdd200 hostapd: unconditionally enable ap/mesh for wpa-cli
    ec6cb33452 mac80211: backport fix for an no-ack tx status issue
    2df0ea042d wpad-wolfssl: fix crypto_bignum_sub()

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=5feb0df9bb3bd83ec49af17d2abc261fc58e2551
+OPENWRT_COMMIT=2df0ea042d14dbfc2735b843eb82a0ae0d8bec19
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
