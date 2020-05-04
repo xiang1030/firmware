910d3b769ca4d3fc4304094b6ad91413e6d35009
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-05-05 00:20:10 +0200

    Update to HEAD of OpenWrt-master
    0e522d5f4a curl: update to version 7.70.0
    b5516603dd mac80211: more wifi reconf related fixes
    abb0452cd8 mediatek: update patches
    220f43e0f2 mediatek: fix image building
    b9cf9ebdb7 mediatek: add uboot
    73de205c57 kernel: bump 5.4 to 5.4.38
    0836d0dea2 odhcpd: update to latest git HEAD (FS#3056)

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=3a79e3b18572d8932f44de43a232e349bf32f37d
+OPENWRT_COMMIT=0836d0dea205a3b65232c28b06774069224f1d50
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
