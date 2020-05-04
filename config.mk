c4f035a0d3850c7143de5f26ec9af31a44e78383
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-05-05 00:10:11 +0200

    Update to HEAD of OpenWrt-19.07 branch
    f9ef0c5705 kernel: bump 4.14 to 4.14.178
    65cf72d5d2 ath79: add SUPPORTED_DEVICES based on ar71xx for some devices
    f40947a8c0 ath79: indicate boot/failsafe/upgrade for NanoBeam/Nanostation AC
    0974d59b5f kernel: backport fix for non-regular inodes on f2fs
    005adba939 mac80211: ath10k: increase rx buffer size to 2048
    5f0e25d966 perf: build with NO_LIBCAP=1
    ee480c50c1 dante: Fix compile with glibc
    55ccb04046 upgs: Remove extra _DEFAULT_SOURCE definition
    96d280cc37 scripts/download: add sources CDN as first mirror
    607809dcdc mac80211: Update to version 4.19.120

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=2df0ea042d14dbfc2735b843eb82a0ae0d8bec19
+OPENWRT_COMMIT=607809dcdc69b1ce90b6eab70222a01b43cedfd4
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
