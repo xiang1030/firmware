ee2cdfe6f2d3f9f26adae37ab41e30c19a285c08
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-05-06 00:20:13 +0200

    Update to HEAD of OpenWrt-master
    9295ce7006 fstools: update to the latest version
    755790808c sunxi: tidy up cases in base-files
    0fb5962e62 sunxi: remove unneeded shebang for preinit files
    4d86e2ce06 buildsystem: add CPE information to ipkg packages and manifest files

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=0836d0dea205a3b65232c28b06774069224f1d50
+OPENWRT_COMMIT=4d86e2ce0686d32dec8051baafe81a179a18cdf4
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
