a9a4d5afc447ded9df66d6d231404e7e0b0a82f4
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-05-07 00:20:10 +0200

    Update to HEAD of OpenWrt-master
    47f17b0662 generic: ar8216: fix unknown packet flooding for ar8229/ar8236
    c3a43753b9 fstools: update to the latest version
    5019a06fc1 ppp: Fix mirror hash

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ath79-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=4d86e2ce0686d32dec8051baafe81a179a18cdf4
+OPENWRT_COMMIT=5019a06fc1027fad35fc9847735218c1057f8483
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
