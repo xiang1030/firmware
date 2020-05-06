ef168c06437f664eaad944d6251155f85dadfee1
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-05-07 00:10:13 +0200

    Update to HEAD of OpenWrt-19.07 branch
    429e4490c4 libpcap: fix library packaging issues
    d9244a1b5b generic: ar8216: fix unknown packet flooding for ar8229/ar8236
    3b9e4d6d4c fstools: update to the latest version
    a6caa8fad1 uhttpd: update to 19.07 Git HEAD
    ac5d5d8d09 ustream-ssl: update to 19.07 Git HEAD
    5e8b50da15 odhcpd: fix lan host reachibility due to identical RIO and PIO prefixes (FS#3056)

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=e678cb15956e99adb3f1b970271824f42a392fbe
+OPENWRT_COMMIT=5e8b50da156f9b8fe1b17fa62a446c0a07734e76
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
